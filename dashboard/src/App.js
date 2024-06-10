084a31ab2f68336154ba9109e7d00e390f3d84ee
Mubashira31 <liferacer1718@gmail.com>
2024-06-10 11:22:06 +0530

    login added

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Login from "./pages/Login/Login";
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route to="/" element={<Dashboard />} />
-    //     <Route to="/login" element={<Login />} />
-    //   </Routes>
-    // </BrowserRouter>
-    // <Login />
-    <Dashboard />
+    <div>
+      <nav>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/board">Board</NavLink>
+      </nav>
+      <Routes>
+        <Route to="/board" element={<Dashboard />} />
+        <Route to="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
--- dashboard/src/pages/Login/Login.js
+++ dashboard/src/pages/Login/Login.js
@@ -1,6 +1,31 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("user-info")) {
+      history("/");
+    }
+  }, []);
+  async function login() {
+    console.warn(email, password);
+    let item = { email, password };
+    let result = await fetch("https://restful-booker.herokuapp.com/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(item),
+    });
+    result = result.json();
+    localStorage.setItem("user-info", JSON.stringify(result));
+    history("/");
+  }
   return (
     <div className="cont">
       <div className="container">
@@ -13,10 +38,19 @@ const Login = () => {
         <div className="button">Sign in with Apple</div>
         <div className="login-form">
           <label className="input-label">Email address</label>
-          <input type="text" className="input-field" />
+          <input
+            type="text"
+            className="input-field"
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label className="input-label">Password</label>
-          <input type="password" className="input-field" />
+          <input
+            type="password"
+            className="input-field"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
+        onClick={login}
         <div className="forgot-password">Forgot password?</div>
         <div className="sign-in-button">
           Sign In
